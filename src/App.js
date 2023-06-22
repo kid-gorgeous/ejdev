c51309878c11f070b7dca28a853e233c13e6930b
evan <evanjulius66@yahoo.com>
2023-06-22 14:34:03 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
+      {/* <-- start header --> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
+
       </header>
+      {/* <-- end header --> */}
     </div>
   );
 }
