9e9cd1b6301c07173e16820f85c3f12d5980d2bd
evan <evanjulius66@yahoo.com>
2023-06-22 21:14:07 -0500

    making things better

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import mountainside from './mountainside.png';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,20 +6,19 @@ function App() {
   return (
 
     <div className="App">
-      {/* <-- start header --> */}
-      <header className="App-header">
-
-        {/* <div className='header-container'>
-          [Link1, Link2, Link3]
-        </div> */}
-
-        {/* Cool react thing to make the page less awkward */}
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
+      
+      <body className="App-body">
         
+        <div className="App-Window">
+          <img src={mountainside} className="App-Window-img" alt="img-window" />
+        </div>
 
         {/* This idk, I just needed it to be there for right now */}
         <p>Hello, I am a part-time developer, and full-stime student.  Welcome to my personal profile. </p>
+        
         <br></br>
+
         <a
           className="App-link"
           href="https://github.com/kid-gorgeous"
@@ -27,11 +27,8 @@ function App() {
         >
           Learn More
         </a>
+      </body>
 
-        
-
-      </header>
-      {/* <-- end header --> */}
 
 
     </div>
