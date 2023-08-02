f958e3cf9b6f73e28121c5fa26305743f47f8894
evan <evanjulius66@yahoo.com>
2023-08-01 21:16:05 -0500

    daily update

--- src/App.js
+++ src/App.js
@@ -53,9 +53,16 @@ function App() {
           <h3>Current focuses</h3>
           <hr></hr>
           <p className='current-paragraph'>
-            Currently I am working on a few projects, but my main concern is acquiring the ability to design,
-            and verify various software and hardware components in systems of modern computer and software designs.
-            -
+            Currently I am working on a few projects, but my main concern is acquiring the ability to design, and verify various software and hardware 
+            components in systems of modern computer and software designs.
+            <br></br>
+            I've acquired a few skills in the past few years, and I'm looking to expand my knowledge in the field of computer science, and software engineering.
+            It is a good of mine to work on projects that are both challenging, and rewarding. I've worked on project using a variety of power languages, and frameworks.
+            Some of these include: C, C++, Python, Java, Javascript, Flask, React, and Node.js. I've also worked with a variety of tools, and frameworks such as: Github, and Docker.
+            <br></br>
+            My most invested work includes the study of sorting algorithms, divide-and-conquer algorithms, and the design of various data structures. My most notable and recent is 
+            TradeAI; an artificial enhanced analysis tool that allows the user to measure and predict the values of various stock options using a variety of machine learning algorithms.
+            <br></br>
           </p>
         </section>
 
@@ -65,7 +72,10 @@ function App() {
           <hr></hr>
           <p className='previous-paragraph'>
             Previously I have worked on a few projects, but the most notable have involved relatively low level programs,
-            terminal REPLs, Single Page Applications, and basic API development.
+            terminal REPLs, Single Page Applications, and basic API development. While enrolled at my university I have learned procedural programming,
+            functional programming, and object oriented programming. I possess a strong understanding of computer architecture, the design process, and 
+            the implementation of various software and hardware components. I have a strong drive to learn, and I am always looking for new opportunities
+            to expand my knowledge in the field of computer science, and software engineering, and to excell in my career.
           </p>
         </section>
 
